1f39916a642f5be88551191d2ea0b0c4c206ec89
Chi-Wei <joeyin@good-series.com>
2024-09-10 09:16:24 -0400

    Add Services page

---
+++ frontend/src/app/services/page.tsx
@@ -0,0 +1,13 @@
+import Description from "./description";
+import OurServices from "./services";
+import Questions from "./questions";
+
+export default function Services() {
+  return (
+    <>
+      <Description />
+      <OurServices />
+      <Questions />
+    </>
+  );
+}
